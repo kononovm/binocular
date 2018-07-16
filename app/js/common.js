4222ffce8b1d5bdf9a5395fed818dc5f4df254ab
kononovm <govardrorkmk@gmail.com>
2018-07-16 19:13:52 +0300

    Добавил сайт по Биноклю

---
+++ app/js/common.js
@@ -0,0 +1,10 @@
+$(function() {
+
+$('.head-galery').owlCarousel({
+    loop: true,
+    nav: true,
+	items: 1,
+	navText: ['<i class="material-icons">keyboard_arrow_left</i>','<i class="material-icons">keyboard_arrow_right</i>']
+})
+
+});
